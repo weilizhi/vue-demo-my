525dd6a1b37e4120d3c2d4e7b7322b5e67be0e04
wlz <674501835@qq.com>
2019-12-18 20:56:56 +0800

    12.18提交

---
+++ wrap/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <FooterGuide></FooterGuide>
+  </div>
+</template>
+
+<script>
+  import FooterGuide from './components/FooterGuide/FooterGuide.vue'
+  export default {
+    name: 'app',
+  components:{
+    FooterGuide
+  }
+  }
+</script>
+
+<style scoped lang="stylus">
+ 
+</style>
---
+++ wrap/src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from "@/router"
+import http from "@/api"
+
+
+
+Vue.prototype.$http = http
+Vue.config.productionTip = false;
+
+new Vue({
+  render: h => h(App),
+  router
+}).$mount('#app');
